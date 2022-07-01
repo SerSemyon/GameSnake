e0ec8a9fbdd2597e8efcfb519706076de3e40300
YtkaYtkaGagaga <sersemyon@gmail.com>
2022-07-01 15:40:59 +0300

    В SnakeController добавлена возможность задать кнопки управления

--- SnakeGame/SnakeController.cs
+++ SnakeGame/SnakeController.cs
@@ -2,30 +2,41 @@
 
 internal class SnakeController
 {
+    Keys upKey = Keys.Up;
+    Keys downKey = Keys.Down;
+    Keys leftKey = Keys.Left;
+    Keys rightKey = Keys.Right;
     Snake snake;
     public SnakeController(Snake snake)
     {
         this.snake = snake;
         FormMain.Instance.KeyDown += new System.Windows.Forms.KeyEventHandler(KeyDown);
     }
+    public SnakeController(Snake snake, Keys[] keys) : this(snake)
+    {
+        upKey = keys[0];
+        downKey = keys[1];
+        leftKey = keys[2];
+        rightKey = keys[3];
+    }
     public void KeyDown(object sender, KeyEventArgs e)
     {
-        switch (e.KeyCode)
+        if (e.KeyCode == upKey)
         {
-            case (Keys.Up):
-                snake.ChangeDirection((0, -1));
-                break;
-            case (Keys.Down):
-                snake.ChangeDirection((0, 1));
-                break;
-            case (Keys.Left):
-                snake.ChangeDirection((-1, 0));
-                break;
-            case (Keys.Right):
-                snake.ChangeDirection((1 ,0));
-                break;
+            snake.ChangeDirection((0, -1));
+        }
+        if (e.KeyCode == downKey)
+        {
+            snake.ChangeDirection((0, 1));
+        }
+        if (e.KeyCode == leftKey)
+        {
+            snake.ChangeDirection((-1, 0));
+        }
+        if (e.KeyCode == rightKey)
+        {
+            snake.ChangeDirection((1, 0));
         }
     }
-
 }
 
